c14cc2dc5b773a8858db436d4e198d724747a892
krivonosoven82 <krivonosoven82@gmail.com>
2017-09-30 17:45:25 +0300

    move files

--- unit-2/Form-style/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('input').change(function() {
-		if($(this).val())
-	    	$(this).addClass('has_value');
-	    else
-	    	$(this).removeClass('has_value');
-	});
-});
